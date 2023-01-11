aa917a4b2155fba182091a38b99ca2ddd635edf4
unknown <textile.zero@gmail.com>
2023-01-12 02:03:28 +0600

    social media get, udpate api added

--- index.js
+++ index.js
@@ -9,21 +9,42 @@ app.use( cors() )
 require( "dotenv" ).config()
 // data turn to json format
 app.use( express.json() )
-
-
-
+// mongodb
 const { MongoClient, ServerApiVersion, ObjectId } = require( 'mongodb' );
 const uri = `mongodb+srv://${ process.env.DB_USER }:${ process.env.DB_PASS }@cluster0.lbqhd62.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient( uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 } );
 
 async function run () {
 	const bannerCollection = client.db( "tour" ).collection( "banner" )
-
+	const socialCollection = client.db( "tour" ).collection( "social" )
+	// home banner
 	app.post( "/updatebanner", async ( req, res ) => {
 		const request = req.body;
 		console.log( request )
-		//const query = {_id : ObjectId(request.id)}
+		const udpateStatus = await bannerCollection.updateMany( { 'status': true }, { "$set": { 'status': false } } )
 		const result = await bannerCollection.insertOne( request );
+		res.send( result )
+	} )
+	app.get( "/banner", async ( req, res ) => {
+		const query = { status: true }
+		const result = await bannerCollection.findOne( query )
+		res.send( result )
+	} )
+	// social 
+	app.get( "/social", async ( req, res ) => {
+		const id = "63bf1137037754a56bd0e41f";
+		const query = { _id: ObjectId( id ) }
+		const result = await socialCollection.findOne( query );
+		res.send( result )
+	} )
+	app.patch( "/social", async ( req, res ) => {
+		const updatedoc = {
+			$set: req.body
+		}
+		const id = "63bf1137037754a56bd0e41f";
+		const filter = { _id: ObjectId( id ) }
+		const result = await socialCollection.updateOne( filter, updatedoc )
+
 		res.send( result )
 	} )
 }
